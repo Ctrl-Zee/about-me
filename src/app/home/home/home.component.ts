edab9a58c19eadfc8535aa73dc1e08dac9709378
andrew_smith_moser <andrew.smith@moserit.com>
2022-01-04 12:32:41 -0500

    Initial commit

---
+++ src/app/home/home/home.component.ts
@@ -0,0 +1,19 @@
+import { AfterContentInit, Component } from '@angular/core';
+import Parallax from 'parallax-js';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent implements AfterContentInit {
+  constructor() {}
+
+  ngAfterContentInit() {
+    const scene = document.getElementById('scene');
+    const parallaxInstance = new Parallax(scene, {
+      relativeInput: true,
+      hoverOnly: true,
+    });
+  }
+}
