ab2c80bf9fa2d0a1db5a33a537a538997a84c99a
andrew_smith_moser <andrew.smith@moserit.com>
2022-01-10 12:30:24 -0500

    Removed scrollbars in all browsers. Added about component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
 import { HomeComponent } from './components/home/home.component';
 import { ParallaxSpaceComponent } from './components/parallax-space/parallax-space.component';
 import { ResizeService } from './services/resize.service';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ParallaxSpaceComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ParallaxSpaceComponent,
+    AboutComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [ResizeService],
   bootstrap: [AppComponent],
